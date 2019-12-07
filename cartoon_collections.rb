6dd129070cc090afbcb657ba880fc35b7ee47a28
Summer Rizzo <srainrizzo@gmail.com>
2019-12-07 23:49:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet (source)
   new
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls  (source)
+  source.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
