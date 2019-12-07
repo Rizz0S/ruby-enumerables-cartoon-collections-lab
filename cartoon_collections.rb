2d206c9dc948aab1a5ee6da741d913219e85a295
Summer Rizzo <srainrizzo@gmail.com>
2019-12-07 23:44:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves (source)
  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(source)
+  new << (source_arr.collect {|str| str.capitalize}) + "!"
+  new
 end
 
 def long_planeteer_calls# code an argument here
