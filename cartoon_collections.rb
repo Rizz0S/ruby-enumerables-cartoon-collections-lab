61f69678354cc383a85db64d8a47460cd25d17b4
Summer Rizzo <srainrizzo@gmail.com>
2019-12-08 00:03:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (source)
+  source.each_with_index do |dwarf, index|
+   puts "#{index + 1}. #{dwarf}"
+ end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (source)
+  new = (source.collect {|str| str.capitalize + "!"})
+  new
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls  (source)
+  source.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (source)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  source.find {|food| cheese_types.include?(food)}
 end
